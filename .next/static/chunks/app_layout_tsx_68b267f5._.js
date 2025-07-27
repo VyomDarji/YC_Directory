39859dee0cafe60313733f9a8fc89398331bdab7
VyomDarji <vyom301004@gmail.com>
2025-07-27 12:52:44 +0530

    Remove .next from tracking

--- .next/static/chunks/app_layout_tsx_68b267f5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f5778023._.css",
-  "static/chunks/node_modules_47507739._.js",
-  "static/chunks/_e3277ea4._.js"
-],
-    source: "dynamic"
-});
